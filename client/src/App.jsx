b59bab8d084960f3006191725d9acf26d86afe06
Diamond Nathaniel <diamondnathaniel2@gmail.com>
2023-12-18 16:36:36 +0100

    completed header UI and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -17,7 +18,9 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md max-h-lg">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,10 +32,19 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to={"/signin"}>
-            <li className=" sm:inline text-slate-700 hover:underline">
-              Sign in
-            </li>
+
+          <Link to={"/profile"}>
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className=" sm:inline text-slate-700 hover:underline">
+                Sign in
+              </li>
+            )}
           </Link>
         </ul>
       </div>
