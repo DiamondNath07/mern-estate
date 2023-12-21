1cb22dedc73cdd93285cd9223c2a68f2dfc3ce6e
Diamond Nathaniel <diamondnathaniel2@gmail.com>
2023-12-21 15:05:14 +0100

    create user update api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import inboxRouter from "./routes/message.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -19,6 +20,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
@@ -27,13 +30,12 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/message", inboxRouter);
 
-
-app.use((err,req,res,next)=>{
-     const statusCode = err.statusCode || 500;
-     const message = err.message || 'Internal Serval Error'
-     return res.status(statusCode).json({
-          success: false,
-          statusCode,
-          message,
-     })
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Serval Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
