71d762de2408e33eede5d9ffaaef28a9883f39ed
Diamond Nathaniel <diamondnathaniel2@gmail.com>
2024-01-16 17:40:25 +0100

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createListing, deleteListing, updateListing, getListings } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing, getListings, getListingss } from "../controllers/listing.controller.js";
 
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -9,7 +9,8 @@ const router = express.Router();
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
-router.get("/getlisting/:id", getListings)
+router.get("/getlisting/:id", getListings);
+router.get('/get', getListingss);
 
 
 
