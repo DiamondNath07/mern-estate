9efb07f9e0b99fdc2ef221274866fa010c27e2d0
Diamond Nathaniel <diamondnathaniel2@gmail.com>
2024-01-17 04:41:16 +0100

    remove cosole log of data

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -65,7 +65,7 @@ export default function Signin() {
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opcaity-95 disabled:opacity-80"
         >
-          {loading ? "loading..." : "sign in"}
+          {loading? "loading..." : "sign in"}
         </button>
         <OAuth/>
       </form>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -14,7 +14,7 @@ export default function Logout() {
     });
   };
 
-  // console.log(formData);
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
