2ef056829e711ee0d1622fc715bcd9254c2b325f
Diamond Nathaniel <diamondnathaniel2@gmail.com>
2024-01-17 05:49:34 +0100

    fix build error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
-import Listing from "./pages/listing";
+import Listing from "./pages/Listing";
 import Search from "./pages/Search";
 
 export default function App() {
