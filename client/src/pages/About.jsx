481fc58d1dbcd572c79dc3db4ce7d98519070f0a
Diamond Nathaniel <diamondnathaniel2@gmail.com>
2024-01-17 11:33:43 +0100

    change text

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
         About Diamond Estate
       </h1>
       <p className="mb-4 text-slate-700">
-        Sahand Estate is a leading real estate agency that specializes in
+        Diamond Estate is a leading real estate agency that specializes in
         helping clients buy, sell, and rent properties in the most desirable
         neighborhoods. Our team of experienced agents is dedicated to providing
         exceptional service and making the buying and selling process as smooth
